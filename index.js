4a5637f6dc78b9485ea48b10b7cc9fe6f9268544
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-05-12 11:47:59 -0400

    updating objects lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee = {
+    name:"Sam",
+    streetAddress: "11 Broadway"
+
+}
+
+
+function updateEmployeeWithKeyAndValue(object,key,value){
+    let copyObj = {...employee}
+    copyObj[key] = value
+    return copyObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (object,key,value){
+    object[key] = value
+    return object
+}
+
+function deleteFromEmployeeByKey(object, key){
+    let copyObj = {...employee}
+    delete copyObj[key]
+    return copyObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key){
+    delete object[key]
+    return object
+}
